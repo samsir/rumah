61786ff6d76240249e93501ffd6388be946c57b4
Samsir <34928886+samsir@users.noreply.github.com>
2023-03-16 23:18:09 +0700

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -9,8 +9,8 @@
 
       for (var i = 0; i < results.length; i++) {  // Iterate over the results
         var item = store[results[i].ref];
-        appendString += '<li class="list-group-item"><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
-        appendString += '<p>' + item.content.substring(0, CONTENT_MAX_LENGTH) + '...</p></li>';
+        appendString += '<a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
+        appendString += '<p>' + item.content.substring(0, CONTENT_MAX_LENGTH) + '...</p>';
       }
 
       searchResults.innerHTML = appendString;
