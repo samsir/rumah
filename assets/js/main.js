e86658dd817d74d0b3e311cd2084b6e253db1f38
Samsir <34928886+samsir@users.noreply.github.com>
2023-04-14 13:53:46 +0700

    tooltip

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-<script>  
-var tooltipList1 = [].slice.call(document.querySelectorAll('[data-bs-toggle = "tooltip"]'))  
-var tooltipList2 = tooltipList1.map(function (tooltipTriggerfun) {  
-  return new bootstrap.Tooltip(tooltipTriggerfun)  
-})  
-</script>  
