87d52a5d94a078d6bc5249ea3053551a8ce4ff1d
Samsir <34928886+samsir@users.noreply.github.com>
2023-05-24 11:55:26 +0700

    ok

--- assets/js/medium.js
+++ assets/js/medium.js
@@ -18,6 +18,7 @@ $(function () {
                 display += `<div class="card-body">`;
                 display += `<h2 class="card-title"><a href="${item.link}">${item.title}</a></h2>`;
                 var yourString = item.description.replace(/<img[^>]*>/g,""); //replace with your string.
+                yourString = yourString.replace('figure', 'p');
                 var maxLength = 123; // maximum number of characters to extract
                 //trim the string to the maximum length
                 var trimmedString = yourString.substr(0, maxLength);
