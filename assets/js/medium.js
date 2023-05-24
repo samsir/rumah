c7ee5112c7a3a3ce50ecb332e02d52d407df6510
Samsir <34928886+samsir@users.noreply.github.com>
2023-05-24 10:53:35 +0700

    ok

--- assets/js/medium.js
+++ assets/js/medium.js
@@ -6,10 +6,9 @@ $(function () {
     };
     $.get(' https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40samsir', data, function (response) {
         if (response.status == 'ok') {
-            $("#logo").append(`<img src="${response.feed["image"]}" class="rounded mx-auto d-block">`)
             var display = '';
             $.each(response.items, function (k, item) {
-                display += `<div class="card mb-3 mx-auto mr-5 " style="width: 20rem;">`;
+                display += `<div class="card mb-3">`;
                 var src = item["thumbnail"]; // use thumbnail url
                 display += `<img src="${src}" class="card-img-top" alt="Cover image">`;
                 display += `<div class="card-body">`;
@@ -17,14 +16,13 @@ $(function () {
                 var yourString = item.description.replace(/<img[^>]*>/g,""); //replace with your string.
                 yourString = yourString.replace('h4', 'p');
                 yourString = yourString.replace('h3', 'p');
-                var maxLength = 120; // maximum number of characters to extract
+                var maxLength = 234; // maximum number of characters to extract
                 //trim the string to the maximum length
                 var trimmedString = yourString.substr(0, maxLength);
                 //re-trim if we are in the middle of a word
                 trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")))
                 display += `<p class="card-text">${trimmedString}...</p>`;
                 
-                display += `<a href="${item.link}" target="_blank" class="btn btn-outline-success" >Read More</a>`;
                 display += '</div></div>';
                 return k < 10;
             });
