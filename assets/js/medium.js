1d966f3a827cccff7e2b3e0abf8921c30ba56d19
Samsir <34928886+samsir@users.noreply.github.com>
2023-05-26 11:25:35 +0700

    ok

--- assets/js/medium.js
+++ assets/js/medium.js
@@ -21,7 +21,7 @@ $(function () {
                 trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")))
                 display += `<p class="card-text">${trimmedString}...</p>`;
                 display += '</div></div></div></div>';
-                return k < 10;
+                return k < 12;
             });
 
             resolve($content.html(display));
@@ -37,7 +37,7 @@ mediumPromise.then(function()
         var pageCount = $(".card").length / pageSize;
 
         for (var i = 0; i < pageCount; i++) {
-            $("#pagin").append(`<li class="page-item"><a class="page-link" href="#">${(i + 1)}</a></li> `);
+            $("#pagin").append(`<ul class="pagination justify-content-center m-0"><li class="page-item"><a class="page-link" href="#">${(i + 1)}</a></li></ul> `);
         }
         $("#pagin li:nth-child(1)").addClass("active");
         showPage = function (page) {
