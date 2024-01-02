44391ab94f3f78df55b580982b18c05c4da373c8
Samsir <34928886+samsir@users.noreply.github.com>
2024-01-02 16:23:57 +0700

    coba

--- assets/js/medium.js
+++ assets/js/medium.js
@@ -1,35 +1,46 @@
 $(function () {
     var mediumPromise = new Promise(function (resolve) {
-    var $content = $('#jsonContent');
-    var data = {
-        rss: 'https://samsir.medium.com/feed'
-    };
-    $.get(' https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fsamsir.medium.com%2Ffeed', data, function (response) {
-        if (response.status == 'ok') {
-            var display = '';
-            $.each(response.items, function (k, item) {
-                display += `<div class="card rounded-0 mb-4"><div class="row g-0"><div class="col-md-3">`;
-                display += `<a href="${item.link}"><img src="${item.thumbnail}" class="img-fluid" alt="Cover image"></a>`;
-                display += `</div>`;
-                display += `<div class="col-md-9"><div class="card-body"><h2 class="card-title"><a href="${item.link}">${item.title}</a></h2>`;
-                var yourString = item.description.replace(/<img[^>]*>/g,""); //replace with your string.
-                var maxLength = 129; // maximum number of characters to extract
-                //trim the string to the maximum length
-                var trimmedString = yourString.substr(0, maxLength);
-                //re-trim if we are in the middle of a word
-                trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")))
-                display += `<p class="card-text">${trimmedString}...</p>`;
-                display += '</div></div></div></div>';
-                return k < 12;
-            });
-
-            resolve($content.html(display));
-        }
+        var $content = $('#jsonContent');
+        var data = {
+            rss_url: 'https://samsir.medium.com/feed'
+        };
+        $.get('https://api.rss2json.com/v1/api.json', data, function (response) {
+            if (response.status == 'ok') {
+                var output = '';
+                $.each(response.items, function (k, item) {
+                    var visibleSm;
+                    if (k < 3) {
+                        visibleSm = '';
+                    } else {
+                        visibleSm = ' visible-sm';
+                    }
+                    output += '<div class="col-sm-6 col-md-4' + visibleSm + '">';
+                    output += '<div class="blog-post"><header>';
+                    output += '<h4 class="date">' + $.format.date(item.pubDate, "dd<br>MMM") + "</h4>";
+                    var tagIndex = item.description.indexOf('<img'); // Find where the img tag starts
+                    var srcIndex = item.description.substring(tagIndex).indexOf('src=') + tagIndex; // Find where the src attribute starts
+                    var srcStart = srcIndex + 5; // Find where the actual image URL starts; 5 for the length of 'src="'
+                    var srcEnd = item.description.substring(srcStart).indexOf('"') + srcStart; // Find where the URL ends
+                    var src = item.description.substring(srcStart, srcEnd); // Extract just the URL
+                    output += '<div class="blog-element"><img class="img-responsive" src="' + src + '" width="360px" height="240px"></div></header>';
+                    output += '<div class="blog-content"><h4><a href="' + item.link + '">' + item.title + '</a></h4>';
+                    output += '<div class="post-meta"><span>By ' + item.author + '</span></div>';
+                    var yourString = item.description.replace(/<img[^>]*>/g, ""); //replace with your string.
+                    var maxLength = 120 // maximum number of characters to extract
+                    //trim the string to the maximum length
+                    var trimmedString = yourString.substr(0, maxLength);
+                    //re-trim if we are in the middle of a word
+                    trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" ")))
+                    output += '<p>' + trimmedString + '...</p>';
+                    output += '</div></div></div>';
+                    return k < 3;
+                });
+                $content.html(output);
+            }
+        });
     });
-    });
 
-mediumPromise.then(function()
-    {
+    mediumPromise.then(function () {
         //Pagination
         pageSize = 4;
 
